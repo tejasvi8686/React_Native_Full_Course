180d44245acc82502c511d8c7d20ded01b286f7e
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-07 17:32:45 +0530

    implememt image error cover

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import ProductListItem from "@/src/components/ProductListItem";
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]} />
+      <ProductListItem product={products[2]} />
       <ProductListItem product={products[1]} />
     </View>
   );
