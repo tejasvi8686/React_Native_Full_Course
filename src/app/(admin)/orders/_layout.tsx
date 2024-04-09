3de279388e572df6a6318f2f265f0d49f3079ba0
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-10 00:09:54 +0530

    top tab naivigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,10 +1,10 @@
-import { Stack} from "expo-router"
-
+import { Stack } from "expo-router";
 
 export default function MenuStack() {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{title: 'Orders'}}/>
-        </Stack>
-    ) 
+  return (
+    <Stack>
+      {/* <Stack.Screen name="index" options={{ title: "Orders" }} /> */}
+      <Stack.Screen name="list" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
