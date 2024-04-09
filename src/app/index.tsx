9d1db0b6e7323201d7cbfc800139b7b185d68a90
tejavi8686 <tejasviraj8686@gmail.com>
2024-04-09 22:34:34 +0530

    signup & and sigin scrren done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,6 +12,10 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
+
+      <Link href={'/sign-in'} asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
